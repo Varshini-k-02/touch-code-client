c4a1c9e666ccd5a03d2288cca94886f81d7984b0
varshini-k-02 <varshinikarthik002@gmail.com>
2024-02-21 11:36:30 +0530

    origin changed

--- index.js
+++ index.js
@@ -17,6 +17,15 @@ app.use(
     allowedHeaders:["Content-Type", "Authorization"]
   })
 );
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "React app URL"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send({ status: "running" });
