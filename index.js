a5b5300895dfa3bd50d69db8a0d5a31276118511
varshini-k-02 <varshinikarthik002@gmail.com>
2024-02-21 11:39:53 +0530

    headers added

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "React app URL"
+    "https://touch-code-frontend.vercel.app/#/*"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
